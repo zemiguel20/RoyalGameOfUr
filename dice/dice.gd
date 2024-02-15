d0e7980966acde2f23a9fca6a736d8479b5409e4
zemiguel20 <jmmscoutinho@gmail.com>
2024-02-15 21:41:54 +0100

    Fixed mock dice multiple clicks

--- dice/dice.gd
+++ dice/dice.gd
@@ -3,18 +3,20 @@ extends Node
 
 
 signal diceClicked
-
+var _selectable = true
 
 ## Mock dice selection
 func _process(delta):
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+	if _selectable and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		diceClicked.emit()
 
 
 ## Mock functions, rolls and returns roll value
 func roll() -> int:
+	_selectable = false
 	print("Playing roll animation...")
 	await get_tree().create_timer(1.0).timeout
 	print("Finished rolling animation...")
+	_selectable = true
 	var roll = randi_range(0, 4)
 	return roll
