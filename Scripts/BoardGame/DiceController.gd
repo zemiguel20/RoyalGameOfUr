7d688215f3b64354843fcbd11eb6f0659b50e50e
Marc313 <77342337+Marc313@users.noreply.github.com>
2024-02-21 13:06:51 +0100

    simulated throwing dice from hand, added customizability and comments to die.gd

--- Scripts/BoardGame/DiceController.gd
+++ Scripts/BoardGame/DiceController.gd
@@ -12,7 +12,13 @@ var total_roll_value = -1;
 signal roll_finished(value : int)
 
 func _ready():
-	random = RandomNumberGenerator.new()
+	initialize_dice()
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func initialize_dice():
 	for i in range(0, numOfDice):
 		var instance = scene.instantiate() as Die
 		add_child(instance)
@@ -20,16 +26,12 @@ func _ready():
 		
 		# TODO: Make this editable as well, but this should be handled by a Placer script.
 		# This script will then also make sure that the dice do not overlapped.
-		var locationX = random.randf_range(-2.5, 2.5)
-		var locationZ = random.randf_range(-2.5, 2.5)
+		var locationX = randf_range(-2.5, 2.5)
+		var locationZ = randf_range(-2.5, 2.5)
 		instance.global_position = Vector3(locationX, 0, locationZ)
 		dice_list.append(instance)
 		instance.input_event.connect(_on_die_input_event)
 		instance.roll_finished.connect(_on_die_roll_finished)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
 func start_roll():
 	total_roll_value = 0
@@ -44,10 +46,6 @@ func start_roll():
 	print("Result: %s" % total_roll_value)
 	return total_roll_value
 
-func _on_die_input_event(camera, event, position, normal, shape_idx):
-	if event is InputEventMouseButton and event.is_pressed():
-		start_roll()
-
 func _on_die_roll_finished(roll_value):
 	num_of_finished_dice += 1
 	total_roll_value += roll_value
@@ -55,3 +53,6 @@ func _on_die_roll_finished(roll_value):
 	if (num_of_finished_dice >= numOfDice):
 		emit_signal("roll_finished", total_roll_value)
 
+func _on_die_input_event(camera, event, position, normal, shape_idx):
+	if event is InputEventMouseButton and event.is_pressed():
+		start_roll()
--- Scripts/BoardGame/Die.gd
+++ Scripts/BoardGame/Die.gd
@@ -1,40 +1,85 @@
 class_name Die
 extends RigidBody3D
 
+@export_category("References")
+## Reference to the parent node containing all raycasts.
+## These raycasts are used to check the value of the dice throw.
 @export var raycasts_parent_node : Node
 
-@export var throwing_force_magnitude = 1.0
-@export var throwing_angular_velocity = 1.0
+@export_category("Throwing Physics")
+## Magnitude of the throwing force applied to this die when starting a roll.
+@export var throwing_force_magnitude : float = 1.0
+## Angular velocity applied to this die when starting a roll.
+@export var throwing_angular_velocity : float = 1.0
+
+## X is min, Y is max
+@export var throwing_force_direction_range_x = Vector2(-1, 1)
+## X is min, Y is max
+@export var throwing_force_direction_range_z = Vector2(-1, 1)
+
+## Height (y-value) of the die when it is released
+@export var throwing_height : float = 2.0
+
+## Not sure how to name this variable yet...
+## Makes sure that when throwing the dice from a hand, the dice spawn in apart from each other.
+## The dice have a random offset from eachother equal to:
+## Vector3(randf_range(-temp_random_offset_on_throw, temp_random_offset_on_throw), 0, randf_range(-temp_random_offset_on_throw, temp_random_offset_on_throw))
+@export var temp_random_offset_on_throw = 1.0
 
 var is_rolling = false
-var raycast_list
+var raycast_list	
+var temp_original_position
 
 signal roll_finished(roll_value : int)
 
 func _ready():
+	temp_original_position = global_position
 	raycast_list = raycasts_parent_node.get_children()
 
 func start_rolling():
+	if (is_rolling):
+		return
+	
 	is_rolling = true
 	
+	# Position the dice as if they just came out of a 'hand'
+	var random_offset = Vector3(randf_range(-temp_random_offset_on_throw, temp_random_offset_on_throw), 0, randf_range(-temp_random_offset_on_throw, temp_random_offset_on_throw))
+	global_position = temp_original_position + random_offset
+	global_position.y = throwing_height
+	
+	# Give the dice a random rotation (Basis) when they exit the 'hand'
+	var random_rotation_x = randf_range(0, 2 * PI)
+	var random_rotation_y = randf_range(0, 2 * PI)
+	var random_rotation_z = randf_range(0, 2 * PI)
+	basis = Basis.from_euler(Vector3(random_rotation_x, random_rotation_y, random_rotation_z))	
+	
 	# Apply force in a random direction
-	var throw_direction = Vector3(randf_range(-1,1), 0, randf_range(-1,1)).normalized()
+	var random_x = randf_range(throwing_force_direction_range_x.x, throwing_force_direction_range_x.y)
+	var random_z = randf_range(throwing_force_direction_range_z.x, throwing_force_direction_range_z.y)
+	var throw_direction = Vector3(random_x, 0, random_z).normalized()
 	var throw_force = throw_direction * throwing_force_magnitude
-	angular_velocity = throw_force * throwing_angular_velocity
+	
 	apply_central_impulse(throw_force)
+	angular_velocity = throw_direction * throwing_angular_velocity
 
+# Triggers when the rigidbody:
+# - Was sleeping but gains velocity
+# - Was not sleeping but no longer has velocity left
 func _on_sleeping_state_changed():
 	if (not is_rolling or not sleeping): return
 	
-	var roll = -1
+	var roll_value = -1
 	for raycast : DiceRaycast in raycast_list:
 		if raycast.is_colliding():
 			print((raycast.get_collider() as Node).name)
-			roll = raycast.opposite_side_value
+			roll_value = raycast.opposite_side_value
 			break
 	
-	if roll == -1:
-		push_error("No value detected ;(")
-	else:
-		emit_signal("roll_finished", roll)
-		is_rolling = false
+	if roll_value == -1:
+		# TODO: Make sure this scenario can never happen.
+		# For example by rolling and 
+		push_warning("No value detected ;(")
+		roll_value = 0
+		
+	emit_signal("roll_finished", roll_value)
+	is_rolling = false
