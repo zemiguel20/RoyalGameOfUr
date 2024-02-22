b951bbb6a991a2444fd9318c0bb2403a3c710ee5
zemiguel20 <jmmscoutinho@gmail.com>
2024-02-22 14:50:40 +0100

    Added move skeleton to Piece, and documentation

--- scripts/piece.gd
+++ scripts/piece.gd
@@ -1,18 +1,27 @@
 class_name Piece
 extends Node
+## Piece of the game. Has selection and highlighting functionality. Also can be physically moved.
 
 
 signal clicked(sender: Piece)
 
 
+## Enables highlighting effects
 func enable_highlight():
 	# TODO: implement
 	pass
 
 
+## Disables highlighting effects
 func disable_highlight():
 	# TODO: implement
 	pass 
+
+
+## Moves the piece physically along the given [param movement_path].
+func move(movement_path: Array[Vector3]):
+	# TODO: implement
+	pass
 
 
 func _on_input_selected():
