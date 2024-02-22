2acd15c471906f993dc72e76ac42a8fbe4da8cf5
Marc313 <77342337+Marc313@users.noreply.github.com>
2024-02-22 18:15:41 +0100

    Added logic for Spot child classes

--- scripts/general.gd
+++ scripts/general.gd
@@ -6,3 +6,11 @@ enum PlayerID {ONE = 1, TWO = 2}
 
 static func get_other_player_id(id: PlayerID) -> PlayerID:
 	return PlayerID.TWO if id == PlayerID.ONE else PlayerID.ONE
+
+
+# Not tested yet.
+static func get_random_position_in_boxshape_3D(shape : BoxShape3D) -> Vector3:
+	var random_position = Vector3()
+	random_position.x = randi_range(shape.position - shape.size.x/2, shape.position + shape.size.x/2)
+	random_position.z = randi_range(shape.position - shape.size.z/2, shape.position + shape.size.z/2)
+	return random_position
