c3ecacacc6d8512f3b8e8a0e2a55ef829a10aa7b
zemiguel20 <jmmscoutinho@gmail.com>
2024-02-28 14:30:15 +0100

    PieceGroup has configurable orientation

--- scripts/game/spot/piece_group.gd
+++ scripts/game/spot/piece_group.gd
@@ -2,20 +2,27 @@ class_name PieceGroup
 extends Node3D
 
 
+@export var _orientation: Orientation = HORIZONTAL
+@export var _spawn_offset: float
+
 var _spots: Array[Spot] = []
 
 
 func setup(size: int) -> void:
 	_spots.resize(size)
+	var dir = Vector3.RIGHT if _orientation == HORIZONTAL else Vector3.UP
 	for i in size:
 		_spots[i] = Spot.new()
 		add_child(_spots[i])
-		_spots[i].global_position = global_position + i * Vector3.RIGHT * 1.5 # TODO: implement actual offset
+		_spots[i].global_position = global_position + i * dir * _spawn_offset
 
 
 func get_available_spot() -> Spot:
 	var free_spots = _spots.filter(func(spot: Spot): return spot.piece == null)
-	return free_spots.pick_random()
+	if _orientation == HORIZONTAL:
+		return free_spots.pick_random()
+	else:
+		return free_spots.front()
 
 
 func get_all_spots() -> Array[Spot]:
