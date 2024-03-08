f0a33a34ea199dd7ae901054f803c1386df78bcf
Marc313 <77342337+Marc313@users.noreply.github.com>
2024-03-08 16:35:23 +0100

    Refactored test for consistency

--- test/scripts/ai/ai_player_advanced_evaluation_test.gd
+++ test/scripts/ai/ai_player_advanced_evaluation_test.gd
@@ -90,34 +90,35 @@ func _test_base_score():
 	var mock_landing_spot = board.get_spot(4, General.PlayerID.TWO) as Spot
 	var normal_move := Move.new(ai_piece, mock_current_spot, mock_landing_spot)
 	
-	var expected1 = ai_player.regular_base_score
-	var result1 = ai_player._calculate_base_score(normal_move)
-	assert(expected1 == result1, "Base score result: %d" % result1)
+	var expected = ai_player.regular_base_score
+	var result = ai_player._calculate_base_score(normal_move)
+	assert(expected == result, "Base score result: %d" % result)
 	
 	# TEST: Capture base score, by moving a player piece.
-	var landing_spot1 = board.get_spot(4, General.PlayerID.ONE)
-	var piece1 = board.get_pieces(General.PlayerID.ONE)[0]
-	await board.move(piece1, landing_spot1)
+	var opponent_landing_spot = board.get_spot(4, General.PlayerID.ONE)
+	var opponent_piece = board.get_pieces(General.PlayerID.ONE)[0]
+	await board.move(opponent_piece, opponent_landing_spot)
 	
-	var expected2 = ai_player.capture_base_score
-	var result2 = ai_player._calculate_base_score(normal_move)
-	assert(expected2 == result2, "Base score result: %d" % result2)
+	expected = ai_player.capture_base_score
+	result = ai_player._calculate_base_score(normal_move)
+	assert(expected == result, "Base score result: %d" % result)
 	
 	# TEST: Grants extra roll base score. In this test, rosette will grant extra rolls.
-	var mock_landing_spot2 = board.get_spot(7, General.PlayerID.TWO) as Spot
-	var rosette_move := Move.new(ai_piece, mock_current_spot, mock_landing_spot2)
+	mock_landing_spot = board.get_spot(7, General.PlayerID.TWO) as Spot
+	var rosette_move := Move.new(ai_piece, mock_current_spot, mock_landing_spot)
 	
-	var expected3 = ai_player.grants_roll_base_score
-	var result3 = ai_player._calculate_base_score(rosette_move)
-	assert(expected3 == result3, "Base score result: %d" % result3)
+	expected = ai_player.grants_roll_base_score
+	result = ai_player._calculate_base_score(rosette_move)
+	assert(expected == result, "Base score result: %d" % result)
 	
 	# TEST: Move to end base score
-	var mock_landing_spot3 = board._p2_end_area.get_available_spot()
-	var end_move := Move.new(ai_piece, mock_current_spot, mock_landing_spot3)
+	mock_landing_spot = board._p2_end_area.get_available_spot()
+	var end_move := Move.new(ai_piece, mock_current_spot, mock_landing_spot)
 	
-	var expected4 = ai_player.end_move_base_score
-	var result4 = ai_player._calculate_base_score(end_move)
-	assert(expected4 == result4, "Base score result: %d" % result4)
+	expected = ai_player.end_move_base_score
+	result = ai_player._calculate_base_score(end_move)
+	assert(expected == result, "Base score result: %d" % result)
+	
 	print("Base score tests completed!")	
 	
 #endregion
@@ -139,19 +140,20 @@ func _test_safety_modifier_to_danger():
 	ai_player.safety_score_weight = 1.0	
 	
 	# TEST: When no opponent pieces are in range, shared spots have a base base_danger_score
-	var expected1 = -ai_player.base_spot_danger
-	var result1 = ai_player._calculate_safety_modifier(move)	
-	assert(expected1 == result1, "Result: %d" % result1)
+	var expected = -ai_player.base_spot_danger
+	var result = ai_player._calculate_safety_modifier(move)	
+	assert(expected == result, "Result: %d" % result)
 	
 	# TEST: 
+	# Move opponent piece
 	var landing_spot1 = board.get_spot(0, General.PlayerID.ONE)
 	var piece1 = board.get_pieces(General.PlayerID.ONE)[0]
 	await board.move(piece1, landing_spot1)
 	
 	# Opponent needs to throw 4 in order to capture, so 1/16 chance.
-	var expected2 = -(ai_player.base_spot_danger + 1.0/16.0)
-	var result2 = ai_player._calculate_safety_modifier(move)	
-	assert(expected2 == result2, "Result: %d" % result2)
+	expected = -(ai_player.base_spot_danger + 1.0/16.0)
+	result = ai_player._calculate_safety_modifier(move)	
+	assert(expected == result, "Result: %d" % result)
 
 	# TEST: When there are 4(MAX) opponent pieces in range
 	for i in range(1,4):
@@ -160,9 +162,9 @@ func _test_safety_modifier_to_danger():
 		await board.move(piece, landing_spot)
 	
 	# Opponent needs to not throw 0 in order to capture, so 15/16 chance.	
-	var expected3 = -(ai_player.base_spot_danger + 15.0/16.0)
-	var result3 = ai_player._calculate_safety_modifier(move)
-	assert(expected3 == result3, "Result: %d" % result3)
+	expected = -(ai_player.base_spot_danger + 15.0/16.0)
+	result = ai_player._calculate_safety_modifier(move)
+	assert(expected == result, "Result: %d" % result)
 	print("Danger safety tests completed!")
 	
 	
@@ -176,26 +178,26 @@ func _test_safety_modifier_to_safety():
 	ai_player.safety_score_weight = 1.0	
 	
 	# TEST: Move from dangerous spot to a safe spot
-	var expected1 = ai_player.base_spot_danger + 15.0/16.0
-	var result1 = ai_player._calculate_safety_modifier(move)
-	assert(expected1 == result1, "Result: %d" % result1)
+	var expected = ai_player.base_spot_danger + 15.0/16.0
+	var result = ai_player._calculate_safety_modifier(move)
+	assert(expected == result, "Result: %d" % result)
 	
 	# TEST: Test moving to a place that is still dangerous, but less dangerous
 	var mock_landing_spot2 = board.get_spot(5, General.PlayerID.TWO) as Spot
-	var move2 := Move.new(ai_piece, mock_current_spot, mock_landing_spot2)
+	var move2 = Move.new(ai_piece, mock_current_spot, mock_landing_spot2)
 	
 	# Danger on new spot is 11/16 + base_danger_score, since opponent can throw 2, 3 or 4
 	# I put the whole calculation here due to float precision
-	var expected2 = (0.1 + 15.0/16.0 - 0.1 - 11.0/16.0)
-	var result2 = ai_player._calculate_safety_modifier(move2)	
-	assert(expected2 == result2, "Result: %d" % result2)
+	expected = (ai_player.base_spot_danger + 15.0/16.0 - ai_player.base_spot_danger - 11.0/16.0)
+	result = ai_player._calculate_safety_modifier(move2)	
+	assert(expected == result, "Result: %d" % result)
 	
 	# TEST: Test going from a shared but safe spot to a 100% safe spot
 	await _reset_pieces_to_start()
 	
-	var expected3 = ai_player.base_spot_danger
-	var result3 = ai_player._calculate_safety_modifier(move)	
-	assert(expected3 == result3, "Result: %d" % result3)
+	expected = ai_player.base_spot_danger
+	result = ai_player._calculate_safety_modifier(move)	
+	assert(expected == result, "Result: %d" % result)
 #endregion	
 	
 #region Progress Score Modifier Tests
