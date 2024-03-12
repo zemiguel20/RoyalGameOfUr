faa7d50163108ba3656758ec3475094262d60557
Marc313 <77342337+Marc313@users.noreply.github.com>
2024-03-12 10:37:11 +0100

    Added logs for testing dice

--- scripts/game/dice/die.gd
+++ scripts/game/dice/die.gd
@@ -47,36 +47,55 @@ func roll() -> void:
 	# Make 100% sure that the die is active, since there was a rare bug that the position was not applied correctly. 
 	visible = true
 	
-	# Position the dice as if they just came out of a 'hand'
+	# Make sure the body is sleeping, so we are allowed to teleport and rotate it.
+	sleeping = true
+	print("Sleeping1: ", sleeping)
+	
+	 #Position the dice as if they just came out of a 'hand'
 	var random_x = randf_range(-_random_dice_offset, _random_dice_offset)
 	var random_z = randf_range(-_random_dice_offset, _random_dice_offset)
 	var random_offset = Vector3(random_x, 0, random_z)
-	self.global_position = _throwing_position + random_offset
+	self.global_position = _throwing_position + Vector3.UP * 2
+	print("Sleeping2: ", sleeping)
+	
 	
 	# Give the dice a random rotation (Basis) when they exit the 'hand'
-	var random_rotation_x = randf_range(-180, 180)
-	var random_rotation_y = randf_range(-180, 180)
-	var random_rotation_z = randf_range(-180, 180)
-	basis = Basis.from_euler(Vector3(random_rotation_x, random_rotation_y, random_rotation_z))	
+	print("Die: ")
+	var random_rotation_x = randf_range(-PI, PI)
+	var random_rotation_y = randf_range(-PI, PI)
+	var random_rotation_z = randf_range(-PI, PI)
+	global_basis = Basis.from_euler(Vector3(random_rotation_x, random_rotation_y, random_rotation_z))	
+	print("Sleeping2: ", sleeping)	
+	#rotate_x(randf_range(-PI, PI))
+	#rotate_y(randf_range(-PI, PI))
+	#rotate_z(randf_range(-PI, PI))
+	
+	#print("Rot: X %s " % (basis.get_euler().x))
+	#print("Rot: Y %s " % (basis.get_euler().y))
+	#print("Rot: Z %s " % (basis.get_euler().z))
+	
+	print("Sleeping3: ", sleeping)	
+	await get_tree().create_timer(2.0).timeout
+	roll_finished.emit(1)	
 	
 	# Apply force in a random direction
-	var random_direction_x = randf_range(throwing_force_direction_range_x.x, throwing_force_direction_range_x.y)
-	var random_direction_z = randf_range(throwing_force_direction_range_z.x, throwing_force_direction_range_z.y)
-	var throw_direction = Vector3(random_direction_x, 0, random_direction_z).normalized()
-	var throw_force = throw_direction * throwing_force_magnitude
-	apply_impulse(throw_force)
-	apply_torque_impulse(throw_direction * throwing_angular_velocity)
+	#var random_direction_x = randf_range(throwing_force_direction_range_x.x, throwing_force_direction_range_x.y)
+	#var random_direction_z = randf_range(throwing_force_direction_range_z.x, throwing_force_direction_range_z.y)
+	#var throw_direction = Vector3(random_direction_x, 0, random_direction_z).normalized()
+	#var throw_force = throw_direction * throwing_force_magnitude
+	#apply_impulse(throw_force)
+	#apply_torque_impulse(throw_direction * throwing_angular_velocity)
 	
 	# Wait a short while before setting _is_rolling to true.
 	# Immediately setting will trigger _on_movement_stopped with sleeping = false,
 	# but since we set _is_rolling the same frame, the function will not return.
-	await get_tree().create_timer(0.1).timeout
-	_is_rolling = true
+	#await get_tree().create_timer(0.1).timeout
+	#_is_rolling = true
 	
-	# A timer specifying a maximum rolling duration.
-	# If the 'rolling' did not stop already, it will stop after the timer and roll again.
-	# Stuck timer prevents infinite waiting for small movements
-	_rolling_timer.start()
+	## A timer specifying a maximum rolling duration.
+	## If the 'rolling' did not stop already, it will stop after the timer and roll again.
+	## Stuck timer prevents infinite waiting for small movements
+	#_rolling_timer.start()
 
 
 # Triggers when the sleeping state of the rigidbody is changed
