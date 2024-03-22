95ac6ece921a08bb0596a6b0f91daf46cd2ddcb3
Marc313 <77342337+Marc313@users.noreply.github.com>
2024-03-22 17:10:27 +0100

    Added Mouse Hover Borders

--- test/camera_lookaround.gd
+++ test/camera_lookaround.gd
@@ -13,15 +13,21 @@ var max_rotation_y: float
 
 var _delta: float
 
+var starting_forward
+var starting_right
+var starting_up
+
 
 func _ready():
-	print("Up vector: ", transform.basis.y)	
-	
 	original_rotation = rotation_degrees
 	min_rotation_x = rotation_degrees.x - 15
 	max_rotation_x = rotation_degrees.x + 15
 	min_rotation_y = rotation_degrees.y - 15
 	max_rotation_y = rotation_degrees.y + 15
+	
+	starting_forward = basis.z
+	starting_right = basis.x
+	starting_up = basis.y
 	
 	
 func _process(delta):
@@ -34,42 +40,50 @@ func _input(event):
 		
 	# Just for testing.
 	if event.keycode == KEY_UP:
-		lookup()
+		look_up()
 	if event.keycode == KEY_DOWN:
-		lookdown()
+		look_down()
 	if event.keycode == KEY_LEFT:
-		lookleft()
+		look_left()
 	if event.keycode == KEY_RIGHT:
-		lookright()
+		look_right()
 		
 
-func lookup():
-	if rotation_degrees.x >= max_rotation_x:
-		return
+func look_up():
+	#if rotation_degrees.x >= max_rotation_x:
+		#return
 		
-	rotation_degrees += rotation_speed * _delta * Vector3.RIGHT
+	#rotation_degrees += rotation_speed * _delta * Vector3.RIGHT
+	rotate_object_local(Vector3.RIGHT, rotation_speed * _delta)		
+	#rotate(starting_right, rotation_speed * _delta)	
 
 
-func lookdown():
-	if rotation_degrees.x <= min_rotation_x:
-		return
+func look_down():
+	#if rotation_degrees.x <= min_rotation_x:
+		#return
 	
-	rotation_degrees += rotation_speed * _delta * -Vector3.RIGHT
-	
-	
-func lookleft():
-	if rotation_degrees.y >= min_rotation_y:
-		return
-	
-	rotation_degrees += rotation_speed * _delta * Vector3.UP
+	#rotation_degrees += rotation_speed * _delta * -Vector3.RIGHT
+	rotate_object_local(Vector3.RIGHT, -rotation_speed * _delta)	
+	#rotate(starting_right, -rotation_speed * _delta)
 	
 	
-func lookright():
-	if rotation_degrees.y <= max_rotation_y:
-		return
+	
+func look_left():
+	#if rotation_degrees.y <= min_rotation_y:
+		#return
+	
+	#rotation_degrees += rotation_speed * _delta * Vector3.UP
+	rotate_object_local(Vector3.UP, rotation_speed * _delta)	
+	#rotate(starting_up, rotation_speed * _delta)		
+	
+func look_right():
+	#if rotation_degrees.y >= max_rotation_y:
+		#return
 
-	rotation_degrees += rotation_speed * _delta * -Vector3.UP
+	#rotation_degrees += rotation_speed * _delta * -Vector3.UP
+	rotate_object_local(Vector3.UP, -rotation_speed * _delta)	
+	#rotate(starting_up, -rotation_speed * _delta)
 	
 	
-func vector_rad_to_deg(vector: Vector3) -> Vector3:
-	return Vector3(rad_to_deg(vector.x), rad_to_deg(vector.y), rad_to_deg(vector.z))
+#func vector_rad_to_deg(vector: Vector3) -> Vector3:
+	#return Vector3(rad_to_deg(vector.x), rad_to_deg(vector.y), rad_to_deg(vector.z))
---
+++ test/mouse_hover_border.gd
@@ -0,0 +1,41 @@
+class_name MouseHoverBorder
+extends ColorRect
+
+enum BorderDirection 
+{
+	Left = 0,
+	Right = 1,
+	Down = 2,
+	Up = 3
+}
+
+@export var direction: BorderDirection
+
+var enabled
+
+func _ready():
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
+
+
+func _process(delta):
+	if not enabled:
+		return
+		
+	print("Enabled for ", direction)
+	if direction == BorderDirection.Left:
+		($"../.." as CameraLookAround).look_left()
+	elif direction == BorderDirection.Right:
+		($"../.." as CameraLookAround).look_right()	
+	elif direction == BorderDirection.Down:
+		($"../.." as CameraLookAround).look_down()	
+	elif direction == BorderDirection.Up:
+		($"../.." as CameraLookAround).look_up()	
+
+
+func _on_mouse_entered():
+	enabled = true
+
+
+func _on_mouse_exited():
+	enabled = false	
