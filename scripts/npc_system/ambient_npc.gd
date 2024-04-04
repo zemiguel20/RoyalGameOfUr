0410e84c96c760770125175dd7918a117dd6fb6c
Marc313 <77342337+Marc313@users.noreply.github.com>
2024-04-04 15:34:57 +0200

    Changed Minor Kitchen Logic

--- scripts/npc_system/ambient_npc.gd
+++ scripts/npc_system/ambient_npc.gd
@@ -14,6 +14,7 @@ var _material: BaseMaterial3D
 var _npc_manager: NPCDataManager
 
 var temp_has_claimed_kitchen
+var is_moving_to_kitchen
 
 func on_ready(manager: NPCDataManager):
 	_npc_manager = manager
@@ -43,11 +44,15 @@ func set_material_color(color: Color):
 
 # Use the NPCManager/Data thing to check conditions like isKitchenClaimed
 func _choose_task() -> NPCTask:
+	if _npc_manager.kitchen.claimer == self:
+		return KitchenTask.new(randf_range(10, 15), self)
+			
 	var random = randi_range(1, 3)
 	if random == 1:
 		return WaitTask.new(randf_range(0.5, 2.5), self)
 	elif random == 2:
 		var pos := Vector3(randf_range(-6.0, 6.0), global_position.y, randf_range(-6.0, 6.0))
+		print("Target Position:", pos)
 		var task = MoveTask.new(pos, self, _nav_agent)
 		_nav_agent.velocity_computed.connect(task.velocity_computed)
 		return task
@@ -56,8 +61,6 @@ func _choose_task() -> NPCTask:
 		if not _npc_manager.kitchen.is_claimed and not temp_has_claimed_kitchen:
 			_npc_manager.kitchen.try_claim(self)
 			temp_has_claimed_kitchen = true
-			return MoveTask.new(_npc_manager.kitchen.global_position, self, _nav_agent)		
-		elif _npc_manager.kitchen.claimer == self:
-			return KitchenTask.new(randf_range(10, 15), self)
-			
+			return MoveTask.new(_npc_manager.kitchen.global_position, self, _nav_agent)	
+
 	return WaitTask.new(1.0, self)
