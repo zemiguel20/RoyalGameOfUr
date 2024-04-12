9f98e8ddfba612eb233088149d7e45b597f0f797
Marc313 <77342337+Marc313@users.noreply.github.com>
2024-04-12 12:36:03 +0200

    Fixed Some Interruption Bugs

--- scripts/dialogue_system/dialogue_group_player.gd
+++ scripts/dialogue_system/dialogue_group_player.gd
@@ -9,6 +9,7 @@ signal on_interruption_ready
 var _current_group: DialogueGroup
 var _current_index = 0
 var _is_interrupted
+var is_playing
 
 @onready var audio_player = $AudioStreamPlayer3D as AudioStreamPlayer
 @onready var subtitle_displayer = $DialogueSubtitles_CanvasLayer as DialogueSubtitles
@@ -21,31 +22,45 @@ func play(group):
 	await on_dialogue_finished
 
 
+## TODO: Check this description
 ## Used for when the dialogue was interrupted after one of the dialogue entries.
 ## Will continue playing the next entry.
 func continue_dialogue():
+	if _current_index >= _current_group.dialogue_entries.size():
+		finish_group()
+		return
+		
+	is_playing = true
 	var _current_entry = _current_group.dialogue_entries[_current_index] as DialogueSingleEntry
+	# FIXME
 	audio_player = $AudioStreamPlayer3D as AudioStreamPlayer3D
 	audio_player.stream = _current_entry.audio
 	audio_player.play()
 	# Show subtitles
-	print(_current_entry.subtitle)
 	subtitle_displayer.display_subtitle(_current_entry.subtitle)
 	var clip_length = _current_entry.audio.get_length()
 	await get_tree().create_timer(clip_length).timeout
 	_current_index += 1
 	
-	if _current_index >= _current_group.dialogue_entries.size():
-		on_dialogue_finished.emit()
-		subtitle_displayer.hide_subtitles()
-		on_interruption_ready.emit()
-	elif _is_interrupted:
-		on_interruption_ready.emit()
-		_is_interrupted = false
-		## TODO: Subtitle displayer should be shared across multiple stuff.
-		subtitle_displayer.hide_subtitles()
+	if _is_interrupted:
+		handle_interruption()
 	else:
 		continue_dialogue()
+		
+		
+func finish_group():
+	on_dialogue_finished.emit()
+	on_interruption_ready.emit()
+	subtitle_displayer.hide_subtitles()
+	is_playing = false
+	
+	
+func handle_interruption():
+	on_interruption_ready.emit()
+	_is_interrupted = false
+	## TODO: Subtitle displayer should be shared across multiple stuff.
+	subtitle_displayer.hide_subtitles()
+	is_playing = false
 	
 	
 func interrupt():
--- scripts/dialogue_system/dialogue_system.gd
+++ scripts/dialogue_system/dialogue_system.gd
@@ -6,8 +6,8 @@ extends Node
 ## List of all dialogues used by this dialog system.
 @export var dialogue_list: Array[DialogueGroup] 
 @export var _temp_interruption: DialogueGroup 
-@onready var dialogue_player = $DialogueGroupPlayer as DialogueGroupPlayer
-@onready var interruption_player = $InterruptionPlayer as DialogueGroupPlayer
+@onready var _dialogue_player = $DialogueGroupPlayer as DialogueGroupPlayer
+@onready var _interruption_player = $InterruptionPlayer as DialogueGroupPlayer
 var _current_index = 0
 
 func play_next():
@@ -17,7 +17,7 @@ func play_next():
 	# Plays the next group of dialogue
 	var next_group = dialogue_list[_current_index]
 	_current_index += 1
-	await dialogue_player.play(next_group)
+	await _dialogue_player.play(next_group)
 	
 	
 func has_next():
@@ -25,8 +25,14 @@ func has_next():
 	
 	
 func interrupt():
-	dialogue_player.interrupt()
-	await dialogue_player.on_interruption_ready
-	await interruption_player.play(_temp_interruption)
+	## TODO: For the prototype this is fine, but we might reconsider this later.
+	## I put a todo just to not forget this part.
+	if _interruption_player.is_playing:
+		return
+	
+	if _dialogue_player.is_playing:
+		_dialogue_player.interrupt()
+		await _dialogue_player.on_interruption_ready
+	await _interruption_player.play(_temp_interruption)
 	## FIXME: If we end with an interruption, we will continue, rather than finish. Not intended.
-	dialogue_player.continue_dialogue()
+	_dialogue_player.continue_dialogue()
