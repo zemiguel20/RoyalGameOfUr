b155a440bc24279ae6f72b56bc4a0636e668a3f2
zemiguel20 <jmmscoutinho@gmail.com>
2024-04-17 16:05:06 +0200

    Move Picker: added backwards move selection
    NOTE: Start spot base highlight is broken, but selection still works

--- scripts/game/move_picker.gd
+++ scripts/game/move_picker.gd
@@ -21,23 +21,12 @@ func _on_move_phase_started(player: General.Player, roll_value: int):
 		
 		(_moves_dict[move.from] as Array).append(move)
 	
-	_enable_from_selection()
-
-
-func _enable_from_selection():
+	# Enable selection
 	for from_spot: Spot in _moves_dict:
 		from_spot.highlight_pieces()
 		from_spot.mouse_entered.connect(_on_spot_hovered.bind(from_spot))
 		from_spot.mouse_exited.connect(_on_spot_dehovered.bind(from_spot))
 		from_spot.selected.connect(_on_from_spot_selected.bind(from_spot))
-
-
-func _disable_from_selection():
-	for from_spot: Spot in _moves_dict:
-		from_spot.dehighlight_pieces()
-		from_spot.mouse_entered.disconnect(_on_spot_hovered.bind(from_spot))
-		from_spot.mouse_exited.disconnect(_on_spot_dehovered.bind(from_spot))
-		from_spot.selected.disconnect(_on_from_spot_selected.bind(from_spot))
 
 
 func _on_spot_hovered(spot: Spot):
@@ -54,13 +43,34 @@ func _on_spot_dehovered(spot: Spot):
 
 func _on_from_spot_selected(spot: Spot):
 	print("click")
-	_disable_from_selection()
+	
+	# Disable selection
+	for from_spot: Spot in _moves_dict:
+		if from_spot != spot:
+			from_spot.dehighlight_pieces()
+		from_spot.mouse_entered.disconnect(_on_spot_hovered.bind(from_spot))
+		from_spot.mouse_exited.disconnect(_on_spot_dehovered.bind(from_spot))
+		from_spot.selected.disconnect(_on_from_spot_selected.bind(from_spot))
 	
 	if not Settings.can_move_backwards:
 		_on_spot_dehovered(spot) # Force dehighlight
+		spot.dehighlight_pieces()
 		var move = _moves_dict[spot].front() as Move
 		await move.execute()
 		move_executed.emit(move)
 	else:
-		# TODO: Implement distinction between only forward selection
-		push_error("No support for backwards selection yet")
+		for move in _moves_dict[spot] as Array[Move]:
+			move.to.selected.connect(_on_to_spot_selected.bind(move))
+
+
+func _on_to_spot_selected(selected_move: Move):
+	# Disable selection
+	for move in _moves_dict[selected_move.from] as Array[Move]:
+		move.to.selected.disconnect(_on_to_spot_selected.bind(move))
+		
+		move.to.dehighlight_base()
+		move.from.dehighlight_base()
+		move.from.dehighlight_pieces()
+	
+	await selected_move.execute()
+	move_executed.emit(selected_move)
