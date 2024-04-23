8da5021d770d25f0e978018a66c37edb5f217151
Marc313 <77342337+Marc313@users.noreply.github.com>
2024-04-23 17:49:34 +0200

    Partly Cleaned camera_lookaround script

--- scripts/looking_around/camera_lookaround_v3.gd
+++ scripts/looking_around/camera_lookaround_v3.gd
@@ -1,56 +1,47 @@
 class_name CameraLookAroundV3
 extends Camera3D
 
-## Big TODO: Fix all names!!
-
 ## Max degrees for camera rotation in all directions. 
-@export var max_degrees: float = 30
-@export var rotation_speed: float = 0.05
-@export var centering_speed: float = 0.5
+@export var max_degrees: float = 35
+## Sensitivity of the mouse in x and y. 
+## TODO Make this a setting in the settings menu of the game in the future
+@export var _looking_sensitivity: float = 0.1
+@export var _centering_speed: float = .9
+## Ask Daniel how he wants to edit this in the inspector.
+## TODO name
 @export var offset_rotation_x: float = -30
-@export var degrees_down_to_trigger: float = 15
-
-@export var enable_feature: bool
-@export var enable_at_game_start: bool
+## TODO name
+@export var degrees_down_to_trigger: float = 23
+## Not sure if ill keep this.
+@export var _enable_feature: bool
 
 @export_group("Self References")
-@export var border_up: Control
+@export var _looking_border: Control
 
-var is_enabled = false
-
-var original_rotation: Vector3
-var min_rotation_x: float
-var max_rotation_x: float
-var min_rotation_y: float
-var max_rotation_y: float
 var _looking_around_rotation: Vector3
-var _board_look_rotation 
+var _board_look_rotation: Vector3
+var _min_rotation_x: float
+var _max_rotation_x: float
+var _min_rotation_y: float
+var _max_rotation_y: float
 
+## Bool indicating whether looking around is currently enabled.
+var _is_enabled = false
+## Cached delta used for rotation in input method.
 var _delta: float
-var _enable_looking = false
 
 
 func _ready():
-	_enable_looking = true
-	is_enabled = false
-	
 	Input.mouse_mode = Input.MOUSE_MODE_CONFINED
-	border_up.mouse_entered.connect(_on_border_up_mouse_entered)
+	_define_main_orientations()
+	_define_constraints()
 	
-	var max_radians = deg_to_rad(max_degrees)
-	original_rotation = global_rotation		# Local rotation
-	_looking_around_rotation = original_rotation
-	_looking_around_rotation.x = 0
-	min_rotation_x = _looking_around_rotation.x - max_radians
-	max_rotation_x = _looking_around_rotation.x + max_radians
-	min_rotation_y = _looking_around_rotation.y - max_radians
-	max_rotation_y = _looking_around_rotation.y + max_radians
-	
-	_board_look_rotation = global_rotation
-	_board_look_rotation.x = deg_to_rad(offset_rotation_x)
-	await get_tree().create_timer(.5).timeout
-	await temp(_board_look_rotation)
-	border_up.visible = true
+	if _enable_feature:
+		await get_tree().create_timer(.5).timeout
+		_return_to_board()
+		_looking_border.mouse_entered.connect(_enter_looking_mode)
+	else:
+		global_rotation = _board_look_rotation
 	
 	
 func _process(delta):
@@ -59,37 +50,59 @@ func _process(delta):
 
 		
 func _input(event):
-	if not enable_feature or not is_enabled:
+	if not _enable_feature or not _is_enabled or not event is InputEventMouseMotion:
 		return
 	
-	if not event is InputEventMouseMotion or not _enable_looking:
-		return
-		
+	# Rotate based on mouse movement.	
 	var mouseMotionEvent = event as InputEventMouseMotion
 	var mouseDelta = mouseMotionEvent.relative as Vector2
+	rotate(Vector3.UP, _looking_sensitivity * _delta * -mouseDelta.x)		# Looking left and right
+	rotate_object_local(Vector3.RIGHT, _looking_sensitivity * _delta * -mouseDelta.y) 	# Looking up and down	
 	
-	rotate(Vector3.UP, rotation_speed * _delta * -mouseDelta.x)
-	rotate_object_local(Vector3.RIGHT, rotation_speed * _delta * -mouseDelta.y)		
-	print(global_rotation_degrees)
-	rotation.x = clampf(rotation.x, min_rotation_x, max_rotation_x)
-	rotation.y = clampf(rotation.y, min_rotation_y, max_rotation_y)
+	# Constrain Rotations
+	rotation.x = clampf(rotation.x, _min_rotation_x, _max_rotation_x)
+	rotation.y = clampf(rotation.y, _min_rotation_y, _max_rotation_y)
 	
-	if rotation.x - original_rotation.x < -deg_to_rad(degrees_down_to_trigger):
+	# If the player looks down at the board, exit looking around mode.
+	if rotation.x - _looking_around_rotation.x < -deg_to_rad(degrees_down_to_trigger):
 		_return_to_board()
 		
 		
-func enable_looking():
-	_enable_looking = true
+func _define_main_orientations():
+	_looking_around_rotation = global_rotation
+	_board_look_rotation = global_rotation
+	_board_look_rotation.x = deg_to_rad(offset_rotation_x)
 	
 	
-func disable_looking():
-	_enable_looking = false
+func _define_constraints():
+	var max_radians = deg_to_rad(max_degrees)
+	_min_rotation_x = _looking_around_rotation.x - max_radians
+	_max_rotation_x = _looking_around_rotation.x + max_radians
+	_min_rotation_y = _looking_around_rotation.y - max_radians
+	_max_rotation_y = _looking_around_rotation.y + max_radians	
+		
+		
+func _return_to_board():
+	_is_enabled = false	
+	await _rotate_with_speed(_board_look_rotation, _centering_speed)
+	
+	Input.mouse_mode = Input.MOUSE_MODE_CONFINED	
+	_looking_border.visible = true	
 
 
-func temp(target_global_euler: Vector3):
-	var duration = target_global_euler.distance_to(global_rotation) / centering_speed
+## Triggered when hovering over the looking around border.
+func _enter_looking_mode():
+	_looking_border.visible = false
+	await _rotate_with_speed(_looking_around_rotation, _centering_speed)
 	
-	# Linear translation of X and Z
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	await Engine.get_main_loop().process_frame
+	_is_enabled = true
+	
+	
+func _rotate_with_speed(target_global_euler: Vector3, speed: float):
+	var duration = target_global_euler.distance_to(global_rotation) / speed
+	
 	var tween_rot = create_tween()
 	tween_rot.bind_node(self).set_parallel(true)
 	tween_rot.tween_property(self, "global_rotation:x", target_global_euler.x, duration)
@@ -97,18 +110,4 @@ func temp(target_global_euler: Vector3):
 	
 	await tween_rot.finished
 	
-		
-func _return_to_board():
-	is_enabled = false	
-	await temp(_board_look_rotation)
-	border_up.visible = true	
-	Input.mouse_mode = Input.MOUSE_MODE_CONFINED	
-
-
-func _on_border_up_mouse_entered():
-	border_up.visible = false
-	await temp(original_rotation)
 	
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	await Engine.get_main_loop().process_frame
-	is_enabled = true
