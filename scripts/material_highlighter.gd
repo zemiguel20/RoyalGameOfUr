3f5b44ee6a6cde7f2adbc172e545ca2f9717b411
zemiguel20 <jmmscoutinho@gmail.com>
2024-05-02 23:33:33 +0200

    Material Highlighter now can target multiple meshes. D4 updated

--- scripts/material_highlighter.gd
+++ scripts/material_highlighter.gd
@@ -9,14 +9,13 @@ extends Node
 @export var highlight_color : Color = Color.WHITE:
 	set(new_value):
 		highlight_color = new_value
-		_update_color(highlight_material)
+		_update_color()
 
-@export var mesh_to_highlight : GeometryInstance3D:
+@export var meshes_to_highlight : Array[GeometryInstance3D]:
 	set(new_instance):
-		# Reset current mesh before updating to new one
-		if mesh_to_highlight:
-			mesh_to_highlight.material_overlay = null
-		mesh_to_highlight = new_instance
+		# Reset current meshes before updating list
+		_update_highlight(false)
+		meshes_to_highlight = new_instance
 		_update_highlight()
 
 @export var active : bool = false:
@@ -25,18 +24,23 @@ extends Node
 		_update_highlight()
 
 
-func _update_highlight():
-	if !mesh_to_highlight:
+func _update_highlight(turn_on : bool = active):
+	if !meshes_to_highlight:
 		return
-	mesh_to_highlight.material_overlay = highlight_material if active else null
+	
+	for mesh in meshes_to_highlight:
+		if mesh != null:
+			mesh.material_overlay = highlight_material if turn_on else null
 
 
 # Recursive update of material color
-func _update_color(mat: Material):
+func _update_color(mat: Material = highlight_material):
 	if !mat:
 		return
 	
 	if mat is ShaderMaterial:
 		mat.set_shader_parameter("color", highlight_color)
+	if mat is StandardMaterial3D:
+		mat.albedo_color = highlight_color
 	
 	_update_color(mat.next_pass)
