5fb3ff6a4a20531a12b02efaf78b2d969d183a9d
Marc313 <77342337+Marc313@users.noreply.github.com>
2024-05-06 15:53:51 +0200

    Reverted num_of_dice to 4

--- scripts/autoloads/game_settings.gd
+++ scripts/autoloads/game_settings.gd
@@ -7,7 +7,7 @@ var num_pieces: int = 7:
 	set(value):
 		num_pieces = clampi(num_pieces, 1, 7)
 ## The number of dice that will be used in the board game.
-var num_dice: int = 1:
+var num_dice: int = 4:
 	set(value):
 		num_dice = clampi(num_dice, 1, 5)
 var can_move_backwards: bool = false
