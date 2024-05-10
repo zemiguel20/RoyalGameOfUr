2cc6bccb812dd51cb6447ccfcf3bd9fe9a85cd3a
zemiguel20 <jmmscoutinho@gmail.com>
2024-05-10 21:40:21 +0200

    Core rework: spot no longer manages pieces

--- scripts/game/spot.gd
+++ scripts/game/spot.gd
@@ -1,104 +1,36 @@
 class_name Spot
 extends Node3D
-## A spot in the board that can hold a piece or a stack of pieces.
-## Contains game logic for placing pieces.
-## Also has highlight effects.
+## A spot in the board. Has properties that define the spot characteristics in game rules.
+## Used as a selection target for the moves. Also has highlight effects.
 
 
-signal pieces_knocked_out(pieces: Array[Piece])
+## Emitted when the mouse enters the spot.
 signal mouse_entered
+## Emitted when the mouse leaves the spot.
 signal mouse_exited
+## Emitted when the spot is clicked/selected.
 signal selected
 
-const PIECE_OFFSET_Y = 0.15
-
+## If true, the pieces in this spot cannot get knocked out.
 @export var is_safe: bool = false
-@export var give_extra_roll: bool = false
-@export var force_allow_stack: bool = false ## If true, always allows stacking, independent of settings
 
-var _pieces: Array[Piece] = []
+## If true, if the player moves to this spot they should get an extra turn.
+@export var give_extra_turn: bool = false
+
+## If true, always allows stacking, independent of settings.
+@export var force_allow_stack: bool = false
 
 @onready var _highlighter := $Highlighter as MaterialHighlighterComponent
 
 
-## Places the new pieces in the spot, with the given animation.
-## Returns a list of knocked out pieces. If no pieces where knocked out, this list is empty.
-func place_pieces(new_pieces: Array[Piece], anim: Piece.MoveAnim) -> Array[Piece]:
-	var knocked_out_pieces: Array[Piece] = []
-	
-	if not can_place(new_pieces):
-		push_error("Cannot move to this spot.")
-		return knocked_out_pieces
-	
-	var player = new_pieces.front().player
-	if not is_occupied(player) and not is_free():
-		knocked_out_pieces = remove_pieces()
-		pieces_knocked_out.emit(knocked_out_pieces)
-	
-	var num_pieces = _pieces.size() # FOR ANIMATION
-	
-	_pieces.append_array(new_pieces)
-	
-	await _place_animation(new_pieces, anim, num_pieces)
-	
-	return knocked_out_pieces
-
-
-func place_piece(new_piece: Piece, anim: Piece.MoveAnim) -> Array[Piece]:
-	return await place_pieces([new_piece], anim)
-
-
-func can_place(pieces: Array[Piece]) -> bool:
-	var player = (pieces.front() as Piece).player
-	
-	# NOTE: Check if any rule is violated, otherwise return true
-	
-	if is_occupied(player) and not force_allow_stack and not is_safe:
-		return false
-	
-	if is_occupied(player) and not force_allow_stack and is_safe and not Settings.can_stack_in_safe_spot:
-		return false
-	
-	if not is_occupied(player) and not is_free() and is_safe:
-		return false
-	
-	return true
-
-
-func is_occupied(player: General.Player) -> bool:
-	return not is_free() and player == _pieces.front().player
-
-
-func is_free() -> bool:
-	return _pieces.is_empty()
-
-
-func get_pieces() -> Array[Piece]:
-	return _pieces.duplicate()
-
-
-func remove_pieces() -> Array[Piece]:
-	var pieces = _pieces.duplicate()
-	_pieces.clear()
-	return pieces
-
-
+## Updates the highlight state. [param active] sets the active state of the effect, and
+## [param color] sets the color of the effect.
 func set_highlight(active : bool, color := Color.WHITE) -> void:
-	if !_highlighter:
+	if not _highlighter:
 		return
+	
 	_highlighter.highlight_color = color
 	_highlighter.active = active
-
-
-func _place_animation(new_pieces: Array[Piece], anim: Piece.MoveAnim, curr_num_pieces: int):
-	var offset = Vector3.UP * PIECE_OFFSET_Y * global_basis.get_scale().y		## Take scale of the board into account!
-	var base_pos = global_position + offset + (curr_num_pieces * offset)
-	for i in new_pieces.size():
-		var piece = new_pieces[i]
-		var target_pos = base_pos + (i * offset)
-		piece.move(target_pos, anim)
-	
-	await get_tree().create_timer(Piece.MOVE_DURATION).timeout
 
 
 func _on_mouse_entered():
@@ -109,8 +41,6 @@ func _on_mouse_exited():
 	mouse_exited.emit()
 
 
-func _on_input_event(_camera, event, _position, _normal, _shape_idx):
-	if event is InputEventMouseButton:
-		event = event as InputEventMouseButton
-		if event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
-			selected.emit()
+func _on_input_event(_camera, event : InputEvent, _position, _normal, _shape_idx):
+	if event.is_action("game_spot_selected"):
+		selected.emit()
