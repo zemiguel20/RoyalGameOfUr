6dd326ea51247ca5eec3fe42a06f2ba69db3741e
zemiguel20 <jmmscoutinho@gmail.com>
2024-05-21 14:51:27 +0200

    Extracted movable component from pieces. adjusted piece scale

--- scripts/game/board/piece.gd
+++ scripts/game/board/piece.gd
@@ -1,27 +1,17 @@
-class_name Piece
-extends Node3D
+class_name Piece extends Node3D
 ## Physical piece in the game. Can be moved with a given animation. Also has highlight effects.
 
 
-## Types of animation for the movement. Animation duration defined in [constant MOVE_DURATION].
-enum MoveAnim {
-	ARC, ## Moves from point A to B in a arc. Uses [constant MOVE_ARC_HEIGHT].
-	LINE, ## Moves linearly from point A to B.
-	NONE, ## No animation. Movement is instantaneous.
-}
-
-## Height offset (in meters) for the ARC animation, 
-## relative to the highest point between 'to' and 'from'.
-const MOVE_ARC_HEIGHT : float = 1.0
-## Movement duration in seconds.
-const MOVE_DURATION : float = 0.4
+signal movement_finished
 
 @onready var _highlighter := $Highlighter as MaterialHighlighterComponent
+@onready var _model = $Model as MeshInstance3D
+@onready var _movable = $Movable as MovableComponent
 
 
 ## Updates the highlight state. [param active] sets the active state of the effect, and
 ## [param color] sets the color of the effect.
-func set_highlight(active : bool, color := Color.WHITE) -> void:
+func set_highlight(active: bool, color := Color.WHITE) -> void:
 	if not _highlighter:
 		return
 	
@@ -31,36 +21,15 @@ func set_highlight(active : bool, color := Color.WHITE) -> void:
 
 ## Moves the piece to the target point [param dest], in global coordinates.
 ## An animation [param anim] can be specified.
-func move(dest : Vector3, anim : MoveAnim = MoveAnim.NONE) -> void:
-	match anim:
-		MoveAnim.ARC:
-			await _move_arc(dest)
-		MoveAnim.LINE:
-			await _move_line(dest)
-		_:
-			global_position = dest
+func move(dest: Vector3, anim := General.MoveAnim.NONE) -> void:
+	_movable.move(dest, anim)
 
 
-func _move_arc(target_pos: Vector3):
-	# Linear translation of X and Z
-	var tween_xz = create_tween()
-	tween_xz.bind_node(self).set_parallel(true)
-	tween_xz.tween_property(self, "global_position:x", target_pos.x, MOVE_DURATION)
-	tween_xz.tween_property(self, "global_position:z", target_pos.z, MOVE_DURATION)
-	
-	# Arc translation of Y
-	var high_point = maxf(global_position.y, target_pos.y) + MOVE_ARC_HEIGHT * global_basis.get_scale().y
-	var tween_y = create_tween().set_trans(Tween.TRANS_CUBIC)
-	tween_y.tween_property(self, "global_position:y", high_point, MOVE_DURATION * 0.5).set_ease(Tween.EASE_OUT)
-	tween_y.tween_property(self, "global_position:y", target_pos.y, MOVE_DURATION * 0.5).set_ease(Tween.EASE_IN)
-	
-	# Tweens run at same time, so only wait for one of them
-	await tween_xz.finished
+## Model height, with global scale applied.
+func get_height_scaled() -> float:
+	var model_bounding_box = _model.mesh.get_aabb() as AABB
+	return model_bounding_box.size.y * _model.global_basis.get_scale().y
 
 
-func _move_line(target_pos: Vector3):
-	var tween = create_tween().bind_node(self)
-	tween.set_trans(Tween.TRANS_QUINT)
-	tween.set_ease(Tween.EASE_OUT)
-	tween.tween_property(self, "global_position", target_pos, MOVE_DURATION)
-	await tween.finished
+func _on_movable_finished():
+	(func(): movement_finished.emit()).call_deferred()
---
+++ scripts/game/components/movable.gd
@@ -0,0 +1,60 @@
+class_name MovableComponent extends Node
+## Provides simple movement animations.
+
+
+signal movement_finished
+
+@export var root_parent: Node3D
+
+## Height offset (in meters) for the arc animation, 
+## relative to the highest point between 'to' and 'from'.
+@export_range(0.0, 100.0, 0.001, "or_greater", "suffix:m")
+var arc_height: float = 1.0
+
+## Movement duration in seconds.
+@export_range(0.0, 5.0, 0.1, "or_greater", "suffix:s")
+var duration: float = 1.0
+
+
+## Moves the target to the target point [param target_pos], in global coordinates.
+## An animation [param anim] can be specified.
+func move(target_pos: Vector3, anim := General.MoveAnim.NONE) -> void:
+	match anim:
+		General.MoveAnim.ARC:
+			move_arc(target_pos)
+		General.MoveAnim.LINE:
+			move_line(target_pos)
+		_:
+			root_parent.global_position = target_pos
+			movement_finished.emit()
+
+
+func move_arc(target_pos: Vector3):
+	# Linear translation of X and Z
+	var tween_xz = create_tween()
+	tween_xz.bind_node(self).set_parallel(true)
+	tween_xz.tween_property(root_parent, "global_position:x", target_pos.x, duration)
+	tween_xz.tween_property(root_parent, "global_position:z", target_pos.z, duration)
+	
+	# Arc translation of Y
+	var current_pos = root_parent.global_position
+	var scale = root_parent.global_basis.get_scale().y
+	var high_point = maxf(current_pos.y, target_pos.y) + arc_height * scale
+	var tween_y = create_tween().set_trans(Tween.TRANS_CUBIC)
+	tween_y.tween_property(root_parent, "global_position:y", high_point, duration * 0.5) \
+	.set_ease(Tween.EASE_OUT)
+	tween_y.tween_property(root_parent, "global_position:y", target_pos.y, duration * 0.5) \
+	.set_ease(Tween.EASE_IN)
+	
+	# Tweens run at same time, so only wait for one of them
+	await tween_xz.finished
+	movement_finished.emit()
+
+
+func move_line(target_pos: Vector3):
+	var tween = create_tween().bind_node(self)
+	tween.set_trans(Tween.TRANS_QUINT)
+	tween.set_ease(Tween.EASE_OUT)
+	tween.tween_property(root_parent, "global_position", target_pos, duration)
+	await tween.finished
+	movement_finished.emit()
