6c10a7baa5f2e4e33e8fc61de2caf32b7ad5ce96
Marc313 <77342337+Marc313@users.noreply.github.com>
2024-05-28 16:21:43 +0200

    Created walk_by_cooldown export var

--- scripts/npc_system/guard_npc.gd
+++ scripts/npc_system/guard_npc.gd
@@ -4,6 +4,7 @@ extends AmbientNPCBase
 #@export var _target_position_storage: Node3D
 @export var _path: Path3D
 @export var _move_speed: float
+@export var _walk_by_cooldown = 10
 
 var _path_points: Array[Vector3]
 var _original_position: Vector3
@@ -25,7 +26,7 @@ func _initialize_tree():
 	## NOTE: For when the guard commentates, we can make a slightly different behaviour tree, 
 	## that uses another path for example, or only the last few points of the path.
 	_current_tree = SequenceNode.new([
-		WaitTask.new(3, self),
+		WaitTask.new(_walk_by_cooldown, self),
 		MoveAlongPathTask.new(_path_points),
 		WarpTask.new(_original_position)
 		])
