25069632e37b1a787a216277e45429e0c2b6fcf4
Marc313 <77342337+Marc313@users.noreply.github.com>
2024-05-31 19:04:16 +0200

    Increased Flexibility of ShowMenuEntry

--- scenes/dialogue_menus.gd
+++ scenes/dialogue_menus.gd
@@ -1,7 +1,18 @@
 class_name DialogueMenuController
 extends CanvasLayer
 
+enum Menu {
+	LookAround = 0,
+}
+
 @onready var _look_around_panel := $MarginContainer as Control
 
-func toggle_look_around_panel(toggle: bool):
+func toggle_menu(menu: Menu, toggle: bool):
+	match menu:
+		Menu.LookAround:
+			_toggle_look_around_panel(toggle)
+		_:
+			push_warning("DialogueMenuController: Menu %s has not been implemented" % menu)
+
+func _toggle_look_around_panel(toggle: bool):
 	_look_around_panel.visible = toggle
--- scripts/dialogue_system/data/dialogue_event.gd
+++ scripts/dialogue_system/data/dialogue_event.gd
@@ -1,11 +1,13 @@
 class_name ShowMenuEntry
 extends DialogueEntry
 
+@export var menu_to_display: DialogueMenuController.Menu
+
 ## Another problem: How do we reference the menu?
 func execute(dialogue_menu_controller: DialogueMenuController):
-	dialogue_menu_controller.toggle_look_around_panel(true)
+	dialogue_menu_controller.toggle_menu(menu_to_display, true)
 	
 	## This condition could be changed to wait until the person is holding right mouse.
 	await dialogue_menu_controller.get_tree().create_timer(3).timeout
 	
-	dialogue_menu_controller.toggle_look_around_panel(false)
+	dialogue_menu_controller.toggle_menu(menu_to_display, false)
