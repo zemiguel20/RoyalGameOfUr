de73fa8320b7401ada134fd916de974df2ee94df
zemiguel20 <jmmscoutinho@gmail.com>
2024-06-02 06:07:22 +0200

    Cleaned MovePicker a bit

--- scripts/game/systems/move_picker/move_picker.gd
+++ scripts/game/systems/move_picker/move_picker.gd
@@ -6,8 +6,6 @@ signal move_executed(move: GameMove)
 
 var selector # Can be of different types
 
-var selected_move: GameMove
-
 
 func _ready() -> void:
 	for node in get_children():
@@ -20,21 +18,16 @@ func _ready() -> void:
 
 ## Starts the selection system for the given list of [param moves].
 func start(moves: Array[GameMove]) -> void:
-	var animation = General.MoveAnim.NONE
-	
 	if selector is AIGameMoveSelector:
 		selector.start_selection(moves)
-		selected_move = await selector.move_selected
+		var selected_move = await selector.move_selected
 		selected_move.execute(General.MoveAnim.ARC, true)
 		await selected_move.execution_finished
 		move_executed.emit(selected_move)
 	
 	if selector is InteractiveGameMoveSelector:
 		selector.start_selection(moves)
-		selected_move = await selector.move_selected
+		var selected_move = await selector.move_selected
 		selected_move.execute(General.MoveAnim.LINE)
 		await selected_move.execution_finished
 		move_executed.emit(selected_move)
-	
-	# Execute move
-	
