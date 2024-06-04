118e65ff9d74b111867478f453efbbe019d74d9b
Marc313 <77342337+Marc313@users.noreply.github.com>
2024-06-04 11:29:13 +0200

    Fixed LookAround Controls Centering issue

--- scenes/dialogue_menus.gd
+++ scenes/dialogue_menus.gd
@@ -5,7 +5,7 @@ enum Menu {
 	LookAround = 0,
 }
 
-@onready var _look_around_panel := $MarginContainer as Control
+@onready var _look_around_panel := $LookAroundControls as Control
 
 func toggle_menu(menu: Menu, toggle: bool):
 	match menu:
--- scripts/dialogue_system/data/dialogue_show_menu_event.gd
+++ scripts/dialogue_system/data/dialogue_show_menu_event.gd
@@ -2,12 +2,12 @@ class_name ShowMenuEntry
 extends DialogueEntry
 
 @export var menu_to_display: DialogueMenuController.Menu
+@export var wait_time: float = 3
 
-## Another problem: How do we reference the menu?
 func execute(dialogue_menu_controller: DialogueMenuController):
 	dialogue_menu_controller.toggle_menu(menu_to_display, true)
 	
 	## This condition could be changed to wait until the person is holding right mouse.
-	await dialogue_menu_controller.get_tree().create_timer(3).timeout
+	await dialogue_menu_controller.get_tree().create_timer(wait_time).timeout
 	
 	dialogue_menu_controller.toggle_menu(menu_to_display, false)
