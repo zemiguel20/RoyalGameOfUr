9f472026ebfd07faf368ede8b81898377e8101d4
José Coutinho <jmmscoutinho@gmail.com>
2024-06-08 21:26:56 +0200

    DiceRoller show result duration and red only if 0 total value (#42)

--- scripts/game/systems/dice_roller.gd
+++ scripts/game/systems/dice_roller.gd
@@ -5,39 +5,40 @@ class_name DiceRoller extends Node3D
 ## Dice are moved to rolling zone first, as if grabbing them.
 
 
-signal dice_placed
+@export var _assigned_player: General.Player
+@export var _impulse_strength: float = 0.002 
+@export var _automatic: bool = false
+@export_range(0.0, 1.0, 0.1, "or_greater") var _show_result_duration: float = 0.5
+@export_range(0.0, 1.0, 0.1, "or_greater") var _auto_delay_grab_dice: float = 0.5
+@export_range(0.5, 1.0, 0.1) var _auto_min_shake_duration: float = 0.5
+@export_range(1.0, 2.0, 0.1) var _auto_max_shake_duration: float = 2.0
 
-@export var assigned_player: General.Player
-@export var automatic: bool = false
-@export var impulse_strength: float = 0.01 
+var _place_spots: Array[Node3D] = []
+var _throw_spots: Array[Node3D] = []
+var _shake_sfx: AudioStreamPlayer3D
+var _dice: Array[Die] = []
 
-var place_spots: Array[Node3D] = []
-var throw_spots: Array[Node3D] = []
-var shake_sfx: AudioStreamPlayer3D
-
-var dice: Array[Die] = []
 
 func _ready() -> void:
-	place_spots.assign(get_node(get_meta("placing_spots")).get_children())
-	throw_spots.assign(get_node(get_meta("throw_spots")).get_children())
-	shake_sfx = get_node(get_meta("shake_sfx"))
+	_place_spots.assign(get_node(get_meta("placing_spots")).get_children())
+	_throw_spots.assign(get_node(get_meta("throw_spots")).get_children())
+	_shake_sfx = get_node(get_meta("shake_sfx"))
 	
-	GameEvents.roll_phase_started.connect(start)
+	GameEvents.roll_phase_started.connect(_start)
 
 
-func start(current_player: General.Player) -> void:
-	if current_player != assigned_player:
+func _start(current_player: General.Player) -> void:
+	if current_player != _assigned_player:
 		return
 	
-	dice.assign(EntityManager.get_dice())
+	_dice.assign(EntityManager.get_dice())
 	_dehighlight()
-	_place_dice()
-	await dice_placed
+	await _place_dice()
 	
-	if automatic:
-		await get_tree().create_timer(0.5).timeout
+	if _automatic:
+		await get_tree().create_timer(_auto_delay_grab_dice).timeout
 		_start_shaking()
-		var shaking_duration = randf_range(0.5, 2.0)
+		var shaking_duration = randf_range(_auto_min_shake_duration, _auto_max_shake_duration)
 		await get_tree().create_timer(shaking_duration).timeout
 		_stop_shaking()
 	else:
@@ -48,23 +49,22 @@ func start(current_player: General.Player) -> void:
 # Moves the dice to the placing spots.
 func _place_dice(skip_animation := false) -> void:
 	# Move dice to random spots
-	place_spots.shuffle()
-	for i in dice.size():
-		var die = dice[i]
-		var spot = place_spots[i]
+	_place_spots.shuffle()
+	for i in _dice.size():
+		var die = _dice[i]
+		var spot = _place_spots[i]
 		
 		var animation = General.MoveAnim.ARC if not skip_animation else General.MoveAnim.NONE
 		die.move_anim.play(spot.global_position, animation)
 	
 	# Make sure dice are completely still
-	for die in dice:
+	for die in _dice:
 		if die.move_anim.moving:
 			await die.move_anim.movement_finished
-	
-	dice_placed.emit()
+
 
 func _connect_input_signals() -> void:
-	for die in dice:
+	for die in _dice:
 		if not die.input.hovered.is_connected(_highlight_hovered):
 			die.input.hovered.connect(_highlight_hovered)
 		if not die.input.dehovered.is_connected(_highlight_selectable):
@@ -78,7 +78,7 @@ func _connect_input_signals() -> void:
 
 
 func _disconnect_input_signals() -> void:
-	for die in dice:
+	for die in _dice:
 		if die.input.hovered.is_connected(_highlight_hovered):
 			die.input.hovered.disconnect(_highlight_hovered)
 		if die.input.dehovered.is_connected(_highlight_selectable):
@@ -92,35 +92,38 @@ func _disconnect_input_signals() -> void:
 
 
 func _highlight_hovered() -> void:
-	for die in dice:
+	for die in _dice:
 		die.highlight.set_active(true).set_color(General.color_hovered)
 
 
 func _highlight_selectable() -> void:
-	for die in dice:
+	for die in _dice:
 		die.highlight.set_active(true).set_color(General.color_selectable)
 
 
-func _highlight_result() -> void:
-	for die in dice:
-		die.highlight.active = true
-		die.highlight.color = General.color_positive if die.value == 1 else General.color_negative
+func _highlight_result(total_value: int) -> void:
+	if total_value == 0:
+		for die in _dice:
+			die.highlight.set_active(true).set_color(General.color_negative)
+	else:
+		for die in _dice:
+			die.highlight.set_active(die.value == 1).set_color(General.color_positive)
 
 
 func _dehighlight() -> void:
-	for die in dice:
+	for die in _dice:
 		die.highlight.active = false
 
 
 func _start_shaking() -> void:
-	shake_sfx.play()
-	for die in dice:
+	_shake_sfx.play()
+	for die in _dice:
 		die.model.visible = false
 
 
 func _stop_shaking() -> void:
-	shake_sfx.stop()
-	for die in dice:
+	_shake_sfx.stop()
+	for die in _dice:
 		die.model.visible = true
 	_roll_dice()
 
@@ -131,22 +134,24 @@ func _roll_dice() -> void:
 	var value = 0
 	
 	# Position and throw dice
-	throw_spots.shuffle()
-	for i in dice.size():
-		var die = dice[i]
-		var throw_spot = throw_spots[i]
+	_throw_spots.shuffle()
+	for i in _dice.size():
+		var die = _dice[i]
+		var throw_spot = _throw_spots[i]
 		
 		var start_position = throw_spot.global_position
 		var start_rotation = General.get_random_rotation()
-		var impulse = throw_spot.global_basis.y * impulse_strength
+		var impulse = throw_spot.global_basis.y * _impulse_strength
 		die.roll(impulse, start_position, start_rotation)
 	
 	# wait roll finished
-	for die in dice:
+	for die in _dice:
 		if die.rolling:
 			await die.roll_finished
 		value += die.value
 	
-	_highlight_result()
+	_highlight_result(value)
+	
+	await get_tree().create_timer(_show_result_duration).timeout
 	
 	GameEvents.rolled.emit(value)
