565c2420a052cbddbbf2cd7143bf21a8850e9297
Marc313 <77342337+Marc313@users.noreply.github.com>
2024-06-11 15:12:08 +0200

    Small Cleanup in NPCSystem Scene

--- scripts/npc_system/npc_system.gd
+++ scripts/npc_system/npc_system.gd
@@ -1,11 +1,12 @@
+class_name NPCSystem
+extends Node
 ## Class managing npcs and keeping track of shared data for npcs.
 ## At this point, this class is not super important, but it would be useful 
 ## for when npcs need to interact with each other or use the same space.
-class_name NPCSystem
-extends Node
 
 @export var dialogue_system: DialogueSystem
 var npcs: Array
+
 
 func _ready():
 	npcs = get_node("NPCs").get_children()
