9974b04e777dc3a6243a4de76e23fdad753dde4a
zemiguel20 <jmmscoutinho@gmail.com>
2024-06-11 17:38:37 +0200

    Moved spawning of pickers to GameController

--- scripts/game/systems/game_controller.gd
+++ scripts/game/systems/game_controller.gd
@@ -2,6 +2,9 @@ class_name GameController extends Node
 ## Sets up the game and controls the flow of the turns.
 
 
+const INTERACTIVE_MOVE_PICKER: PackedScene = preload("res://scenes/game/systems/move_picker/move_picker_interactive.tscn")
+const AI_MOVE_PICKER: PackedScene = preload("res://scenes/game/systems/move_picker/move_picker_ai.tscn")
+
 var current_player: General.Player
 
 
@@ -13,9 +16,13 @@ func _ready():
 
 
 func start_game():
+	# Despawn stuff
 	EntityManager.despawn_board()
 	EntityManager.despawn_dice()
+	for move_picker in get_children():
+		move_picker.queue_free()
 	
+	# Spawn stuff according to settings
 	var board = EntityManager.spawn_board(Settings.ruleset.board_layout.scene)
 	
 	for i in Settings.ruleset.num_pieces:
@@ -25,6 +32,14 @@ func start_game():
 	for i in Settings.ruleset.num_dice:
 		EntityManager.spawn_die()
 	
+	var p1_move_picker = INTERACTIVE_MOVE_PICKER.instantiate()
+	add_child(p1_move_picker)
+	var p2_move_picker = INTERACTIVE_MOVE_PICKER.instantiate() if Settings.is_hotseat_mode \
+			else AI_MOVE_PICKER.instantiate()
+	p2_move_picker.assigned_player = General.Player.TWO
+	add_child(p2_move_picker)
+	
+	# Start game
 	current_player = randi_range(General.Player.ONE, General.Player.TWO) as General.Player
 	GameEvents.game_started.emit()
 	GameEvents.roll_phase_started.emit(current_player)
