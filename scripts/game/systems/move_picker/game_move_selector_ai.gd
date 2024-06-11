d5e3d77e7413f9c984cbc2e92c1e72f4b29e8b66
Daniel <dansebmey@gmail.com>
2024-06-11 13:11:34 +0200

    Make opponent moves faster, especially in Fast Mode

--- scripts/game/systems/move_picker/game_move_selector_ai.gd
+++ scripts/game/systems/move_picker/game_move_selector_ai.gd
@@ -47,9 +47,12 @@ signal move_selected(move: GameMove)
 
 
 func start_selection(moves: Array[GameMove]) -> void:
-	# Simulate thinking
-	var thinking_duration = randf_range(min_moving_duration, max_moving_duration)
-	await get_tree().create_timer(thinking_duration).timeout
+	if not Settings.fast_move_enabled:
+		# Simulate thinking
+		var thinking_duration = fixed_moving_duration
+		if fixed_moving_duration == -1:
+			thinking_duration = randf_range(min_moving_duration, max_moving_duration)
+		await get_tree().create_timer(thinking_duration).timeout
 	var selected_move = _determine_next_move(moves)
 	
 	# Highlight selected move for a bit
