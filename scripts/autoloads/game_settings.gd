83df4b31c916316585d8693e5c84c6449af61aaa
zemiguel20 <jmmscoutinho@gmail.com>
2024-06-11 17:11:13 +0200

    Dice roller automatic changes depending if hotseat

--- scripts/autoloads/game_settings.gd
+++ scripts/autoloads/game_settings.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var is_hotseat_mode: bool = false
 
 var ruleset: Ruleset = preload("res://resources/rulesets/ruleset_finkel.tres")
+var is_hotseat_mode: bool = false
 var fast_move_enabled: bool = false
