62bce07b09e31139e97c4470e785ca0428939f52
zemiguel20 <jmmscoutinho@gmail.com>
2024-12-17 16:46:27 +0100

    Spawn dice

--- scripts/game/dice.gd
+++ scripts/game/dice.gd
@@ -1,7 +1,16 @@
 class_name Dice
 extends Node
+## Manages a group of dice.
+
+
+const DIE_PREFAB: PackedScene = preload("res://scenes/game/entities/d4_die.tscn")
+
+var dice: Array[Die] = []
 
 
 func init(num_dice: int, spawn: DiceZone) -> void:
-	# TODO: implement
-	push_error("Function not implemented.")
+	var spawn_points = spawn.get_placing_points_global_randomized()
+	for i in num_dice:
+		var die = DIE_PREFAB.instantiate()
+		add_child(die)
+		die.global_position = spawn_points[i]
--- scripts/game/dice_zone.gd
+++ scripts/game/dice_zone.gd
@@ -5,3 +5,11 @@ extends Node3D
 
 @onready var _dice_placing_spots: Node3D = $DicePlacingSpots
 @onready var _dice_throw_spots: Node3D = $DiceThrowSpots
+
+
+func get_placing_points_global_randomized() -> Array[Vector3]:
+	var spots = _dice_placing_spots.get_children()
+	var points: Array[Vector3] = []
+	points.assign(spots.map(func(spot: Node3D): return spot.global_position))
+	points.shuffle()
+	return points
