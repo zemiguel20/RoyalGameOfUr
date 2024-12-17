a6cd12f1665347dcc106f0fb78e0ef9cfba31f56
zemiguel20 <jmmscoutinho@gmail.com>
2024-12-17 16:45:50 +0100

    Created dice zone scene

--- scripts/game/dice_zone.gd
+++ scripts/game/dice_zone.gd
@@ -1,2 +1,7 @@
 class_name DiceZone
 extends Node3D
+## Container of points where the dice can be placed and thrown from.
+
+
+@onready var _dice_placing_spots: Node3D = $DicePlacingSpots
+@onready var _dice_throw_spots: Node3D = $DiceThrowSpots
