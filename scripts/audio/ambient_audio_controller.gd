12a5b8271fa7dc101998920f2c199f9142f8ae15
zemiguel20 <jmmscoutinho@gmail.com>
2025-01-24 12:55:36 +0100

    Added ambient sound controller to level

---
+++ scripts/audio/ambient_audio_controller.gd
@@ -0,0 +1,30 @@
+extends Node
+
+# TODO: add fade out audio when scene exit
+
+const FADE_DURATION: float = 1.0 # seconds
+
+
+func _ready() -> void:
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Ambient"), -80)
+	_fade_in_audio()
+
+
+func _fade_in_audio() -> void:
+	await _fade_audio(0.0)
+
+
+func _fade_out_audio() -> void:
+	await _fade_audio(-80)
+
+
+# Decrease audio NOTE: Not compatible with tweeners
+func _fade_audio(target_volume: float) -> void:
+	var time = 0.0
+	var current_volume = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Ambient"))
+	while time < FADE_DURATION:
+		var new_volume = lerpf(current_volume, target_volume, time / FADE_DURATION)
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Ambient"), new_volume)
+		await Engine.get_main_loop().process_frame
+		time += get_process_delta_time()
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Ambient"), target_volume)
