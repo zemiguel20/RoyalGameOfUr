3023f3fe2618d59c81f08ecb6620e535722046c1
zemiguel20 <jmmscoutinho@gmail.com>
2025-01-24 20:50:38 +0100

    Reworked main

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,76 +1,99 @@
-class_name Main extends Node
+extends Node
 
 
-@export var skip_intro := false
+signal level_loaded
 
-@export var loading_screen_fade_duration: float = 0.5
-@export var loading_delay: float = 1.0 ## Delay to give time for scene loading
+const LEVEL_SCENE_PATH: String = "res://scenes/game.tscn"
 
-@export_group("References")
-@export var splash_screen: SplashScreen
-@export var start_menu: StartMenu
-@export var loading_screen: Control
+@export var _skip_intro := false
 
-@onready var level: Node3D = $Level
-@onready var level_scene: PackedScene = preload("res://scenes/game.tscn")
+var _level: Level
+var _loading_level: bool = false
+
+@onready var _splash_screen: SplashScreen = $SplashScreen
+@onready var _title_screen: TitleScreen = $TitleScreen
+@onready var _loading_screen: LoadingScreen = $LoadingScreen
+@onready var _start_menu: StartMenu = $StartMenu
 
 
 func _ready() -> void:
-	GameEvents.back_to_main_menu_pressed.connect(_on_back_to_main_menu)
+	# INFO: The level is loading during the splash screen
 	
-	loading_screen.show()
-	await get_tree().create_timer(loading_delay).timeout # Delay to allow loading
-	loading_screen.hide()
+	_load_level_async()
+	_loading_screen.show()
 	
-	if skip_intro:
-		start_menu.show()
-	else:
-		splash_screen.play_splash_screen_sequence()
+	_start_menu.hide()
+	
+	if not _skip_intro:
+		_splash_screen.play()
+		await _splash_screen.finished
+	
+	if _loading_level:
+		await level_loaded
+	
+	await _loading_screen.fade_out()
+	
+	_title_screen.play()
+	await _title_screen.pressed
+	
+	_start_menu.show()
+	var game_config = await _start_menu.play_pressed
+	
+	_start_menu.hide()
+	_level.start_game(game_config)
 
 
-func _on_back_to_main_menu() -> void:
-	var volume = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master"))
+func _load_level_async() -> void:
+	ResourceLoader.load_threaded_request(LEVEL_SCENE_PATH, "PackedScene", true)
+	_loading_level = true
 	
-	loading_screen.visible = true
-	loading_screen.modulate.a = 0.0
+	var status = ResourceLoader.load_threaded_get_status(LEVEL_SCENE_PATH)
+	while status == ResourceLoader.ThreadLoadStatus.THREAD_LOAD_IN_PROGRESS:
+		await get_tree().create_timer(0.1).timeout
+		status = ResourceLoader.load_threaded_get_status(LEVEL_SCENE_PATH)
 	
-	# Fade in loading screen and fadeout audio
-	var animator = create_tween()
-	animator.tween_property(loading_screen, "modulate:a", 1.0, loading_screen_fade_duration)
-	_fade_audio(-80, loading_screen_fade_duration)
-	await animator.finished
+	var level_scene = ResourceLoader.load_threaded_get(LEVEL_SCENE_PATH) as PackedScene
+	_level = level_scene.instantiate()
+	add_child(_level)
 	
-	# Reload level
-	level.queue_free()
-	await Engine.get_main_loop().process_frame
-	level = level_scene.instantiate()
-	add_child(level)
-	if not level.is_node_ready():
-		await level.ready
+	# HACK: gives some time for lighting and stuff to load
+	await get_tree().create_timer(1.0).timeout
 	
-	await get_tree().create_timer(loading_delay).timeout # Delay to allow proper loading
-	
-	# Fade out loading screen and fade in audio
-	animator = create_tween()
-	animator.tween_property(loading_screen, "modulate:a", 0.0, loading_screen_fade_duration)
-	_fade_audio(volume, loading_screen_fade_duration)
-	await animator.finished
-	
-	loading_screen.visible = false
-	
-	start_menu.show_with_fade()
+	level_loaded.emit()
+	_loading_level = false
 
 
-# Decrease audio NOTE: Not compatible with tweeners
-func _fade_audio(target_volume: float, duration: float) -> void:
-	var time = 0.0
-	var current_volume = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master"))
-	while time < duration:
-		var new_volume = lerpf(current_volume, target_volume, time / duration)
-		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), new_volume)
-		await Engine.get_main_loop().process_frame
-		time += get_process_delta_time()
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), target_volume)
+#func _on_back_to_main_menu() -> void:
+	#var volume = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master"))
+	#
+	#loading_screen.visible = true
+	#loading_screen.modulate.a = 0.0
+	#
+	## Fade in loading screen and fadeout audio
+	#var animator = create_tween()
+	#animator.tween_property(loading_screen, "modulate:a", 1.0, loading_screen_fade_duration)
+	#_fade_audio(-80, loading_screen_fade_duration)
+	#await animator.finished
+	#
+	## Reload level
+	#level.queue_free()
+	#await Engine.get_main_loop().process_frame
+	#level = level_scene.instantiate()
+	#add_child(level)
+	#if not level.is_node_ready():
+		#await level.ready
+	#
+	#await get_tree().create_timer(loading_delay).timeout # Delay to allow proper loading
+	#
+	## Fade out loading screen and fade in audio
+	#animator = create_tween()
+	#animator.tween_property(loading_screen, "modulate:a", 0.0, loading_screen_fade_duration)
+	#_fade_audio(volume, loading_screen_fade_duration)
+	#await animator.finished
+	#
+	#loading_screen.visible = false
+	#
+	#start_menu.show_with_fade()
 
 
 # NOTE: ONLY FOR TESTING
