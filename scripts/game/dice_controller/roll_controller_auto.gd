b9ee31728cd1488773987070126b7b1c07a373b5
zemiguel20 <jmmscoutinho@gmail.com>
2025-01-26 22:00:35 +0100

    Implemented automatic dice roll

--- scripts/game/dice_controller/roll_controller_auto.gd
+++ scripts/game/dice_controller/roll_controller_auto.gd
@@ -1,3 +1,16 @@
 class_name AutoRollController
 extends RollController
-# TODO: implement
+## Automatically shakes the dice for a certain duration and tosses.
+
+
+const DELAY_GRAB_DICE: float = 0.5
+const MIN_SHAKE_DURATION: float = 0.5
+const MAX_SHAKE_DURATION: float = 2.0
+
+
+func _start_toss_procedure() -> void:
+	await get_tree().create_timer(DELAY_GRAB_DICE).timeout
+	_start_shaking()
+	var shake_duration = randf_range(MIN_SHAKE_DURATION, MAX_SHAKE_DURATION)
+	await get_tree().create_timer(shake_duration).timeout
+	_stop_shaking()
