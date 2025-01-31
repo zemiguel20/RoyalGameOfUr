cea33f0d23f2a0bcd26aba4b821010c19e9650a2
zemiguel20 <jmmscoutinho@gmail.com>
2025-01-31 20:11:24 +0100

    Added pausing the game

--- scripts/level/level.gd
+++ scripts/level/level.gd
@@ -9,6 +9,8 @@ signal exited
 @onready var _opponent: OpponentNPC = $Opponent_Guard_Animated
 @onready var _ambient_audio: AmbientAudioController = $AmbientAudio
 @onready var _end_screen: EndScreen = $EndScreen
+@onready var _controls_hud: ControlsHUD = $ControlsHUD
+@onready var _pause_menu: PauseMenu = $PauseMenu
 
 @onready var _start_pov: Camera3D = $CameraPOVS/StartPOV
 @onready var _singleplayer_intro_pov: Camera3D = $CameraPOVS/SingleplayerIntroPOV
@@ -16,21 +18,31 @@ signal exited
 @onready var _hotseat_pov: Camera3D = $CameraPOVS/HotseatPOV
 
 
-
 func _ready() -> void:
 	_game_camera.move_to_POV(_start_pov.global_transform)
 	_game_camera.can_look_around = false
+	
 	_opponent.visible = false
 	_opponent.init(_board_game)
-	_end_screen.hide()
+	
+	_controls_hud.hide()
+	
+	_pause_menu.hide()
+	_pause_menu.can_pause = false
+	_pause_menu.quit_pressed.connect(_on_exit_requested)
+	_pause_menu.paused.connect(_on_game_paused)
+	_pause_menu.resume_pressed.connect(_on_game_resumed)
 	
 	_board_game.ended.connect(_on_game_ended)
+	
+	_end_screen.hide()
 	_end_screen.rematch_pressed.connect(_on_rematch_requested)
 	_end_screen.exit_pressed.connect(_on_exit_requested)
 
 
 func start_game(config: BoardGame.Config) -> void:
 	_board_game.setup(config)
+	_controls_hud.init(_board_game)
 	
 	if config.hotseat:
 		_game_camera.move_to_POV(_hotseat_pov.global_transform)
@@ -44,13 +56,17 @@ func start_game(config: BoardGame.Config) -> void:
 		_opponent.enable_reactions()
 		_game_camera.can_look_around = true
 	
+	_controls_hud.show()
 	_board_game.start()
+	_pause_menu.can_pause = true
 
 
 func _on_game_ended(winner: BoardGame.Player) -> void:
 	if not _board_game.config.hotseat:
 		_opponent.stop()
+	_controls_hud.hide()
 	_end_screen.show_end_menu(winner, _board_game.config.hotseat)
+	_pause_menu.can_pause = false
 
 
 func _on_rematch_requested() -> void:
@@ -66,4 +82,16 @@ func _on_exit_requested() -> void:
 		_opponent.stop()
 	_game_camera.move_to_POV(_start_pov.global_transform)
 	_ambient_audio.fade_out_audio()
+	_controls_hud.hide()
+	_pause_menu.hide()
 	exited.emit()
+
+
+func _on_game_paused() -> void:
+	_controls_hud.hide()
+	_pause_menu.show()
+
+
+func _on_game_resumed() -> void:
+	_pause_menu.hide()
+	_controls_hud.show()
