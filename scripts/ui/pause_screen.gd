69ef2745eb5a7f463ea5edf487939161935d5446
zemiguel20 <jmmscoutinho@gmail.com>
2025-02-10 12:20:27 +0100

    Added settings menu to pause screen

--- scripts/ui/pause_screen.gd
+++ scripts/ui/pause_screen.gd
@@ -14,11 +14,17 @@ var _is_paused: bool = false
 
 @onready var _resume_button: Button = $TabletPanel/Buttons/ResumeButton
 @onready var _main_menu_button: Button = $TabletPanel/Buttons/MainMenuButton
+@onready var _settings_button: Button = $TabletPanel/Buttons/SettingsButton
+@onready var _settings_menu: SettingsMenu = $SettingsMenu
 
 
 func _ready():
+	_settings_menu.hide()
+	
 	_resume_button.pressed.connect(_on_resume_pressed)
 	_main_menu_button.pressed.connect(_on_quit_pressed)
+	_settings_button.pressed.connect(_on_settings_pressed)
+	_settings_menu.back_pressed.connect(_on_settings_back_pressed)
 
 
 func _input(event: InputEvent) -> void:
@@ -48,3 +54,13 @@ func _on_resume_pressed():
 func _on_quit_pressed():
 	_resume()
 	quit_pressed.emit()
+
+
+func _on_settings_pressed():
+	hide()
+	_settings_menu.show()
+
+
+func _on_settings_back_pressed():
+	show()
+	_settings_menu.hide()
